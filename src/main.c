b1fbf78e0982733f26f80d404715380da01a098b
Lionel Alves Vieira <lionel.alves-vieira@etu.u-paris.fr>
2024-11-28 16:34:39 +0100

    Commentaires et cleanup main.c/main.h

--- src/main.c
+++ src/main.c
@@ -1,47 +1,15 @@
 #include "main.h"
-#include <sys/wait.h>
-#include <unistd.h>
-
-int execute_external_command(char *cmd, char **args) {
-    pid_t pid = fork();
-
-    if (pid == -1) {
-        perror("fork");
-        return 1; 
-    }
-
-    if (pid == 0) {
-        // Dans le processus fils, on exécute la commande
-        if (execvp(cmd, args) == -1) { 
-            perror("execvp");
-            exit(1);
-        }
-    } else {
-        // Processus père
-        int status;
-        waitpid(pid, &status, 0); // Attend que le fils se termine
-
-        if (WIFEXITED(status)) {
-            return WEXITSTATUS(status);
-        } else {
-            return 1; 
-        }
-    }
-
-    return 0;
-}
-
-
 
 int main() {
-    char *line; // Stock la ligne lue par l'utilisateur
+    char *line; // Ligne lue depuis l'entrée utilisateur
     int last_return = 0; // Code de retour de la dernière commande
     char *tokens[MAX_TOKENS];  // Tableau pour stocker les tokens
-    int nb_tokens = 0;  // Compteur de tokens
+    int nb_tokens = 0;  // Nombre de tokens extraits
 
     rl_outstream = stderr; // Redirige la sortie de readline vers stderr
 
     while (1) {
+        // Génère et affiche le prompt
         char *prompt = generate_prompt(last_return);
 
         line = readline(prompt);
@@ -50,34 +18,32 @@ int main() {
             break;
         }
 
-        if (line == NULL || line[0] == '\0' || strspn(line, " \t") == strlen(line)) {
+        //Ignore les lignes vides
+        if (line[0] == '\0' || strspn(line, " \t") == strlen(line)) {
             free(line);
             continue;
         }
 
-
-
-        add_history(line); // Ajoute la ligne à l'historique des commandes
+        add_history(line); // Ajoute la commande à l'historique
         cleanup_tokens(tokens, &nb_tokens); // Reinitialisation des tokens
 
-        // Copier la ligne lue avant l'appel à tokenizer
+        // Duplication de la ligne pour éviter de modifier l'originale
         char *copy_line = strdup(line);
-        if (!copy_line) { // Vérifier l'échec de strdup
+        if (!copy_line) {
             perror("Erreur de copie de la ligne");
             free(line);
-            continue;  // Continue avec la prochaine itération
+            continue;
         }
 
-        // Appel de la fonction tokenizer pour remplir le tableau de tokens
+        // Tokenisation de la ligne
         tokenizer(copy_line, tokens, &nb_tokens, " ");
-
         free(copy_line);
 
-        // Sépare la ligne en commande et argument
+        // Séparation commande / argument
         char *command = strtok(line, " ");
         char *arg = strtok(NULL, " ");
 
-        if (command == NULL) { // Si aucune commande n'est saisie
+        if (!command) {
             free(line);
             continue;
         }
@@ -88,6 +54,19 @@ int main() {
             continue;;
         }
 
+        // Gestion des commandes externes
+        else { 
+            // Préparation des arguments pour execvp
+            char *argv[MAX_TOKENS + 1] = {command};
+            for (int i = 1; i < nb_tokens; i++) {
+                argv[i] = tokens[i];
+            }
+            argv[nb_tokens] = NULL;
+
+            // Exécute la commande et met a jour la valeur de retour
+            last_return = execute_external_command(command, argv);
+        }
+
         //Gestion des boucles for
         if (strcmp(command, "for") == 0) {
             // Variables pour stocker la syntaxe de la boucle
@@ -96,20 +75,19 @@ int main() {
             char *rep = strtok(NULL, " "); // Le répertoire
 
             if (var && in && rep && strcmp(in, "in") == 0) {
-                // Utilisation de strtok pour découper la partie de la commande entre { et }
-                char *cmd_start = strtok(NULL, "{"); // Trouver la première partie avant '{'
+                // Utilisation de strtok pour découper la partie de la commande entre '{' et '}'
+                char *cmd_start = strtok(NULL, "{");
                 if (cmd_start != NULL) {
-                    // Chercher le reste de la commande après la fermeture de la commande '}'.
                     cmd_start++; // Décaler pour ignorer le '{'
 
-                    // Chercher la fermeture avec '}'
+                    // Chercher la fermeture avec '}'et la remplacer par '\0' pour terminer la commande
                     char *cmd_end = strchr(cmd_start, '}');
                     if (cmd_end != NULL) {
-                        *cmd_end = '\0'; // Remplace '}' par '\0' pour terminer la commande
+                        *cmd_end = '\0'; 
 
                         // Nettoyer cmd_start pour enlever les espaces superflus
                         while (*cmd_start == ' ' || *cmd_start == '\t') {
-                            cmd_start++; // Ignorer les espaces au début de la commande
+                            cmd_start++;
                         }
 
                         // Vérifie la syntaxe de la commande (variable, mot-clé "in", répertoire)
@@ -134,22 +112,10 @@ int main() {
             }
         }
 
-        // Gestion des commandes externes
-        else { 
-            // Préparation des arguments pour execvp
-            char *argv[MAX_TOKENS + 1] = {command};
-            for (int i = 1; i < nb_tokens; i++) {
-                argv[i] = tokens[i];
-            }
-            argv[nb_tokens] = NULL;
-
-            // Appel à la fonction pour exécuter la commande externe
-            last_return = execute_external_command(command, argv);
-        }
-
         free(line);
     }
 
+    cleanup_tokens(tokens, &nb_tokens); 
     exit(last_return); 
     return 0;
 }

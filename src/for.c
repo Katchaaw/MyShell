f4541033fafa4d6d4d5f94a2012ca5862fea840d
Coulombel <corentin.coulombel@etu.u-paris.fr>
2024-12-18 14:52:09 +0100

    détection des options simple pour la boucle for

--- src/for.c
+++ src/for.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
-int fsh_for(const char *rep, const char *cmd) {
+int fsh_for(const char *rep, const char *cmd,const char *opt) {
     //printf("DEBUG: Début de la boucle 'for' avec répertoire : |%s| et commande : |%s|\n", rep, cmd);
-    
+    printf("option for : [%s]",opt);
     // Ouverture du répertoire
     DIR *dir = opendir(rep);
     if (dir == NULL) {
@@ -35,10 +35,17 @@ int fsh_for(const char *rep, const char *cmd) {
 #define MAX_CMD_LENGTH 1024 // Taille maximale de la commande
 
 int handle_for(char *arg, int *last_return) {
+    int have_opt = 0;
     char *var = arg;               // La variable F
     char *in = strtok(NULL, " ");  // Le mot-clé "in"
     char *rep = strtok(NULL, " "); // Le répertoire
     printf("rep : %s\n",rep);
+    char *opt = strtok(NULL," ");
+    printf("opt : |%s|\n",opt);
+        
+    if (strcmp(opt, "-A")==0 || strcmp(opt, "r")==0){
+        have_opt = 1;
+    }   
 
     if (var && in && rep && strcmp(in, "in") == 0) {
         char *cmd_start = strtok(NULL, "}"); // On récupère le début après la première '{'
@@ -47,7 +54,9 @@ int handle_for(char *arg, int *last_return) {
             *last_return = 1;
             return 1;
         }
-        cmd_start++;
+        if (have_opt){
+            cmd_start++;
+        }
         //printf("cmd_start : %s\n",cmd_start);
 
         char full_command[MAX_CMD_LENGTH] = {0}; // Pour la commande complète
@@ -75,7 +84,8 @@ int handle_for(char *arg, int *last_return) {
         printf("DEBUG: Commande extraite pour la boucle : %s \n", cmd_final);
 
         if (strlen(var) == 1) {
-            *last_return = fsh_for(rep, cmd_final);
+            //strcat(full_command, "\0");
+            *last_return = fsh_for(rep, cmd_final,opt);
         } else {
             fprintf(stderr, "Syntaxe incorrecte: for F in REP { CMD }\n");
             *last_return = 1;
--- src/include/for.h
+++ src/include/for.h
@@ -8,7 +8,7 @@
  * @param cmd La commande à exécuter pour chaque fichier.
  * @return 0 si la commande a été exécutée pour tous les fichiers, 1 en cas d'erreur.
  */
-int fsh_for(const char *rep, const char *cmd);
+int fsh_for(const char *rep, const char *cmd,const char *opt);
 
 
 int handle_for(char *arg, int *last_return);

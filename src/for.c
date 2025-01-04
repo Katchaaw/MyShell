ab6d40c7e3dca755d52a3c827eb0774a85567067
Coulombel <corentin.coulombel@etu.u-paris.fr>
2025-01-04 15:29:14 +0100

    prototype de parallélisation pour for

--- src/for.c
+++ src/for.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
-int fsh_for(const char *rep, const char *cmd,int opt_A, int opt_r,const char *opt_ext,char opt_type,char variable) { 
-    int last_return=0;  
+int fsh_for(const char *rep, const char *cmd, int opt_A, int opt_r, const char *opt_ext, char opt_type, char variable, int opt_p, int max_p) { 
+    int last_return = 0;  
+    int active_processes = 0; // Nombre de processus actifs
 
     // Ouverture du répertoire
     DIR *dir = opendir(rep);
@@ -42,10 +43,9 @@ int fsh_for(const char *rep, const char *cmd,int opt_A, int opt_r,const char *op
         if (opt_r) {
             struct stat file_stat;
             if (stat(filepath, &file_stat) == 0 && S_ISDIR(file_stat.st_mode)) {
-                fsh_for(filepath, cmd, opt_A, opt_r, opt_ext, opt_type,variable);
+                fsh_for(filepath, cmd, opt_A, opt_r, opt_ext, opt_type, variable, opt_p, max_p);
             }
         }
-
 
         // Gestion de l'option -t (filtrage par type de fichier)
         if (opt_type) {
@@ -61,19 +61,50 @@ int fsh_for(const char *rep, const char *cmd,int opt_A, int opt_r,const char *op
             else if (S_ISLNK(file_stat.st_mode)) type_char = 'l';  // Lien symbolique
             else if (S_ISFIFO(file_stat.st_mode)) type_char = 'p'; // FIFO
 
-            if (type_char != opt_type ){
+            if (type_char != opt_type) {
                 continue;
             }
         }
 
-        // Exécuter la commande pour le fichier/répertoire courant
-        int last_returnTemp = execute_command(cmd, filepath, filepath,variable);
-        if (last_returnTemp> last_return){last_return = last_returnTemp;}
+        // Gestion de l'option -p (traitement parallèle)
+        if (opt_p) {
+            // Attendre si le nombre de processus actifs atteint max_p
+            while (active_processes >= max_p) {
+                wait(NULL); // Attendre qu'un processus enfant se termine
+                active_processes--;
+            }
 
+            pid_t pid = fork();
+            if (pid == 0) {
+                // Processus enfant
+                exit(execute_command(cmd, filepath, filepath, variable));
+            } else if (pid > 0) {
+                // Processus parent
+                active_processes++;
+            } else {
+                perror("Erreur lors de la création du processus");
+                closedir(dir);
+                return 1;
+            }
+        } else {
+            // Exécution séquentielle si -p n'est pas activé
+            int last_returnTemp = execute_command(cmd, filepath, filepath, variable);
+            if (last_returnTemp > last_return) {
+                last_return = last_returnTemp;
+            }
+        }
     }
+
+    // Attendre la fin de tous les processus enfants
+    while (active_processes > 0) {
+        wait(NULL);
+        active_processes--;
+    }
+
     closedir(dir);
     return last_return;
 }
+
 
 
 int handle_for(char *arg, int *last_return) {
@@ -83,6 +114,8 @@ int handle_for(char *arg, int *last_return) {
     int opt_r = 0; // Option pour activer la récursion
     char *ext = ""; // Extension à filtrer
     char *type0 = ""; // Type de fichier à filtrer
+    int opt_p = 0;
+    int max_p =0;
     char *var = arg;
     char *in = strtok(NULL, " ");
     char *rep = strtok(NULL, " "); // Répertoire à parcourir
@@ -101,6 +134,10 @@ int handle_for(char *arg, int *last_return) {
         }
         if (strcmp(opt, "-t")==0){     
             type0 = strtok(NULL, " ");
+        }
+        if (strcmp(opt, "-p")==0){     
+            max_p = atoi(strtok(NULL, " "));
+            opt_p = 1;
         }
         opt = strtok(NULL," ");
     }
@@ -139,7 +176,7 @@ int handle_for(char *arg, int *last_return) {
 
         // Vérification de la longueur de la variable
         if (strlen(var) == 1) {
-            *last_return = fsh_for(rep, cmd_final,opt_A,opt_r,ext,*type0,*arg);
+            *last_return = fsh_for(rep, cmd_final,opt_A,opt_r,ext,*type0,*arg,opt_p,max_p);
         } 
         else {
             fprintf(stderr, "Syntaxe incorrecte: for F in REP { CMD }\n");
